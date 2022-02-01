3164a796272803803d3ac5f643c2ea720407b603
domnit bogdan <bogdan.domnit92@gmail.com>
2022-02-01 15:25:34 +0200

    New function add build

--- functions.py
+++ functions.py
@@ -2,12 +2,30 @@ def cleanup(string):
     """Sterge spatile de la inceput si sfarsit
     Transforma stringul in formatul prima litera mare, urmatoarele mici
     Returneaza stringul
+    :param string: un string care va fi transformate
+    :returns: stringul fara spatii si cu prima litera mare
     """
-    new_string = string.strip()
+    new_string = string.strip()  # elimina spatile de la inceput si sfarsit
     new_string = new_string.capitalize()
     return string
 
 
-print(cleanup( " text de transformat"))
+def newf(first, second):
+    """Adds two value
+    Parameters
+        first: primul parametru care va fi adunat  
+        second: al doilea parametru care va fi adunat
+    Returns:
+        Suma celor doi parametrii
+
+    """
+    return first + second
+
+
+print(cleanup(" text de transformat"))
 print(cleanup('test'))
-print(cleanup( 'un text'))
+print(cleanup('un text'))
+print(cleanup("un text de schimbat in functia cleanup"))
+print(cleanup.__doc__)
+help(cleanup)
+help(add)
